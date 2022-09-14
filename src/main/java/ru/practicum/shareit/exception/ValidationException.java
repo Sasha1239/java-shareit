32bb9e3c73234bbca26a8afef500498e91c22063
Sasha1239 <ham1239@gmail.com>
2022-09-14 21:30:24 +0300

    refactor: для прохождения checkstyle

--- src/main/java/ru/practicum/shareit/exception/ValidationException.java
+++ src/main/java/ru/practicum/shareit/exception/ValidationException.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
 @ResponseStatus(HttpStatus.CONFLICT)
 public class ValidationException extends IllegalArgumentException {
     public ValidationException(String message) {
--- src/main/java/ru/practicum/shareit/item/repository/ItemRepositoryImpl.java
+++ src/main/java/ru/practicum/shareit/item/repository/ItemRepositoryImpl.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.item.repository;
 
 import org.springframework.stereotype.Component;
-import ru.practicum.shareit.item.dto.ItemMapper;
 import ru.practicum.shareit.item.model.Item;
 
 import java.util.*;
